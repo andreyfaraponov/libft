9b4325b7ed1263a6011c028000b93fdce2d4e4b9
Andrey FARAPONOV <afarapon@e2r14p2.unit.ua>
2017-11-05 13:33:13 +0200

    one more change for strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: afarapon <afarapon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/28 16:14:07 by afarapon          #+#    #+#             */
-/*   Updated: 2017/10/29 15:33:41 by afarapon         ###   ########.fr       */
+/*   Updated: 2017/11/05 02:52:23 by afarapon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,16 @@ size_t		ft_strlcat(char *dest, const char *src, size_t n)
 	size_t		size;
 
 	size = (size_t)(ft_strlen(dest) + ft_strlen((char*)src));
-	if (ft_strlen(dest) > n)
+	if (ft_strlen(dest) >= n)
 		return (ft_strlen((char*)src) + n);
 	n = n - ft_strlen(dest) - 1;
 	while (*dest)
 		dest++;
-	while (n--)
+	while (*(unsigned char*)src && n)
+	{
 		*(unsigned char*)dest++ = *(unsigned char*)src++;
+		n--;
+	}
 	*dest = '\0';
 	return (size);
 }
